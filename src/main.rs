cc4db567ad500a00d4be668b265f2ebb92c1e089
Corey Farwell <coreyf@rwell.org>
2017-03-11 15:18:15 -0500

    Move response into a dedicated function.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use iron::status;
 use std::io::Read;
 use std::fs;
 
+fn respond(req: &mut Request) -> Result<Response, IronError> {
+    let file = fs::File::open("index.html").expect("could not read from file");
+    let bytes = file.bytes().map(|b| b.unwrap()).collect::<Vec<_>>();
+    let mut response = Response::with((status::Ok, bytes));
+    response.headers.set(iron::headers::ContentType(iron::mime::Mime(iron::mime::TopLevel::Text, iron::mime::SubLevel::Html, vec![])));
+    Ok(response)
+}
+
 fn main() {
-    Iron::new(|_: &mut Request| {
-        let file = fs::File::open("index.html").expect("could not read from file");
-        let bytes = file.bytes().map(|b| b.unwrap()).collect::<Vec<_>>();
-        let mut response = Response::with((status::Ok, bytes));
-        response.headers.set(iron::headers::ContentType(iron::mime::Mime(iron::mime::TopLevel::Text, iron::mime::SubLevel::Html, vec![])));
-        Ok(response)
-    }).http("localhost:3000").unwrap();
+    Iron::new(respond).http("localhost:3000").unwrap();
 }
