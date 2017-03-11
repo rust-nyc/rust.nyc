5886b28c877951fe95b5192349885b8ed0a0dd6d
Corey Farwell <coreyf@rwell.org>
2017-03-11 15:22:21 -0500

    More extraction.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 extern crate iron;
 
 use iron::prelude::*;
+use iron::mime;
 use iron::status;
 use std::io::Read;
 use std::fs;
+use std::path;
 
-fn respond(req: &mut Request) -> Result<Response, IronError> {
-    let file = fs::File::open("index.html").expect("could not read from file");
+fn static_file_response<P: AsRef<path::Path>>(path: P) -> Response {
+    let file = fs::File::open(path.as_ref()).expect("could not read from file");
     let bytes = file.bytes().map(|b| b.unwrap()).collect::<Vec<_>>();
     let mut response = Response::with((status::Ok, bytes));
-    response.headers.set(iron::headers::ContentType(iron::mime::Mime(iron::mime::TopLevel::Text, iron::mime::SubLevel::Html, vec![])));
+    response.headers.set(iron::headers::ContentType(mime::Mime(mime::TopLevel::Text, mime::SubLevel::Html, vec![])));
+    response
+}
+
+fn respond(req_: &mut Request) -> Result<Response, IronError> {
+    let response = static_file_response("index.html");
     Ok(response)
 }
 
